30cf0ff436c4bc8fd03619469adf41b2d0598d92
w4ngzhen <w4ngzhen@hotmail.com>
2025-04-15 10:15:06 +0800

    init

---
+++ rspack.config.cjs
@@ -0,0 +1,53 @@
+const { defineConfig } = require('@rspack/cli');
+const rspack = require('@rspack/core');
+const path = require('path');
+
+module.exports = defineConfig({
+  mode: 'development',
+  entry: {
+    main: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[j|t]sx?/,
+        use: ['babel-loader'],
+        exclude: /node_moudles/,
+      },
+      {
+        test: /\.css$/,
+        use: [rspack.CssExtractRspackPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          rspack.CssExtractRspackPlugin.loader,
+          'css-loader',
+          {
+            loader: 'less-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      inject: 'body',
+    }),
+    new rspack.CssExtractRspackPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    host: '0.0.0.0',
+  },
+});
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { h } from 'preact';
+import * as styles from './App.module.less';
+
+export const App = () => {
+  return <div className={styles.app}>hello, world.</div>;
+};
